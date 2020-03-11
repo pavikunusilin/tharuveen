56f5dfb5c1aa11e25f374c6f44a788652d4f80de
pavi304 <pavi.kunusilin@live.com>
2020-03-12 00:16:45 +0800

    Changed index layout + added navbar to home and guestbook page

---
+++ components/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav
+    class="max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-5xl mx-auto w-full pt-8"
+  >
+    <div class="lg:flex lg:items-center lg:justify-left py-2 px-8 lg:px-0">
+      <ul
+        class="pt-8 lg:pt-0 lg:flex lg:items-center lg:justify-center lg:w-2/4 hidden"
+      >
+        <li
+          class="block py-2 lg:px-4 flex lg:justify-center group cursor-pointer z-50"
+        >
+          <a
+            href="/journey"
+            title="Journey"
+            class="text-black hover:text-blue-500"
+          >
+            Journey
+          </a>
+        </li>
+        <li
+          class="block py-2 lg:px-4 flex lg:justify-center group cursor-pointer z-50"
+        >
+          <a
+            href="/gallery"
+            title="gallery"
+            class="text-black hover:text-blue-500"
+          >
+            Gallery
+          </a>
+        </li>
+        <li
+          class="block py-2 lg:px-4 flex lg:justify-center group cursor-pointer z-50"
+        >
+          <a
+            href="/guestbook"
+            title="Guestbook"
+            class="text-black hover:text-blue-500"
+          >
+            Guestbook
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
--- pages/guestbook.vue
+++ pages/guestbook.vue
@@ -1,54 +1,67 @@
 <template>
-  <form class="w-full flex flex-col items-center justify-center">
-    <h1 class="text-center text-3xl font-semibold py-6">
-      Guestbook
-    </h1>
-    <div class="md:flex md:items-center mb-6">
-      <div class="md:w-1/3">
-        <label
-          class="text-center block text-gray-800 font-bold md:text-right mb-1 md:mb-0 pr-4"
-          for="inline-full-name"
-        >
-          Name
-        </label>
+  <div class="container">
+    <navbar />
+    <form class="w-full flex flex-col items-center justify-center">
+      <h1 class="text-center text-3xl font-semibold py-6">
+        Guestbook
+      </h1>
+      <div class="md:flex md:items-center mb-6">
+        <div class="md:w-1/3">
+          <label
+            class="text-center block text-gray-800 font-bold md:text-right mb-1 md:mb-0 pr-4"
+            for="inline-full-name"
+          >
+            Name
+          </label>
+        </div>
+        <div class="md:w-2/3">
+          <input
+            id="guest-name"
+            class="bg-white appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-400"
+            type="text"
+            placeholder="Your dear name sir/ma'am"
+          />
+        </div>
       </div>
-      <div class="md:w-2/3">
-        <input
-          id="guest-name"
-          class="bg-white appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-400"
-          type="text"
-          placeholder="Your dear name sir/ma'am"
-        />
+      <div class="md:flex md:items-center mb-6">
+        <div class="md:w-1/3">
+          <label
+            class="text-center block text-gray-800 font-bold md:text-right mb-1 md:mb-0 pr-4"
+            for="inline-username"
+          >
+            Message
+          </label>
+        </div>
+        <div class="md:w-2/3">
+          <textarea
+            id="inline-username"
+            class="bg-white appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-400"
+            type="password"
+            placeholder="Your lovely message to the bride & groom"
+          />
+        </div>
       </div>
-    </div>
-    <div class="md:flex md:items-center mb-6">
-      <div class="md:w-1/3">
-        <label
-          class="text-center block text-gray-800 font-bold md:text-right mb-1 md:mb-0 pr-4"
-          for="inline-username"
-        >
-          Message
-        </label>
+      <div class="md:flex md:items-center">
+        <div class="md:w-1/3"></div>
+        <div class="md:w-2/3">
+          <button
+            class="shadow bg-red-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded md:flex items-center"
+            type="button"
+          >
+            Send some love!
+          </button>
+        </div>
       </div>
-      <div class="md:w-2/3">
-        <textarea
-          id="inline-username"
-          class="bg-white appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-400"
-          type="password"
-          placeholder="Your lovely message to the bride & groom"
-        />
-      </div>
-    </div>
-    <div class="md:flex md:items-center">
-      <div class="md:w-1/3"></div>
-      <div class="md:w-2/3">
-        <button
-          class="shadow bg-red-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded md:flex items-center"
-          type="button"
-        >
-          Send some love!
-        </button>
-      </div>
-    </div>
-  </form>
+    </form>
+  </div>
 </template>
+
+<script>
+import Navbar from '~/components/Navbar.vue'
+
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
